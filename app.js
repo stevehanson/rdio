201f3cc038af0fba8bcd7017d5bed72e1e3feff4
Steve Hanson <s.hanson5@gmail.com>
2014-06-18 20:32:14 -0500

    Was using wrong request param from slack

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ var rdioClient = new RdioClient('k78q9x9858wfexvyq5jqc9p8', 'G8aNvrfwcR');
 
 app.get('/current/song', function(req, res){
 
-  console.log('command is: ' + req.param('command'));
+  console.log('command is: ' + req.param('text'));
+  var user =  req.param('text') || 'stevehans';
 
-  var user =  getUserFromCommand(req.param('command')) || 'stevehans';
-
-  rdioClient.getLastSongPlayed('stevehans', function(err, result) {
+  rdioClient.getLastSongPlayed(user, function(err, result) {
 
     if(err) {
       res.send("Oh no! We couldn't get the last song for " + user + ":( Error: " + err);
@@ -23,12 +22,6 @@ app.get('/current/song', function(req, res){
   });
 
 });
-
-// cmd is of form "/rdio {username}"
-function getUserFromCommand(cmd) {
-  var params = cmd.trim().split(' ');
-  return (params.length > 1) ? params[1] : null;
-}
 
 var port = Number(process.env.PORT || 9000);
 var server = app.listen(port, function() {
